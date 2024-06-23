8bb8c81047bce0fdcea4e27c84f795ce1326496f
artsemlemesh <126878429+artsemlemesh@users.noreply.github.com>
2024-06-23 11:35:26 +0800

    copy

---
+++ frontend/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3001,
+    strictPort: true,
+  },
+  server: {
+    port: 3001,
+    strictPort: true,
+    host: true,
+    origin: 'http://0.0.0.0:3001'
+  
+  },
+})
